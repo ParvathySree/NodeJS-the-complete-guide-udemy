b0858311f235d179dd7597172e5d2a788473e0c4
ParvathySree <sreeparvathylaiju@gmail.com>
2024-05-06 17:05:57 +0530

    Redirecting requests

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const http = require('http');
+const fs = require('fs')
 
 const server = http.createServer((req,res)=>{
-    console.log(req.url,req.method,req.headers);
-    if(req.url==='/'){
+    const url = req.url
+    const method = req.method
+    if(url==='/'){
         res.setHeader('Content-Type','text/html');
         res.write('<html>')
         res.write('<head><title>Node App</title><head>')
@@ -10,6 +12,13 @@ const server = http.createServer((req,res)=>{
         res.write('</html>')
         return res.end();
     }
+    if(url==='/message' && method==='POST'){
+        fs.writeFileSync('message.txt','DUMMY')
+        res.statusCode = 302;
+        res.setHeader('Location','/')
+        return res.end();
+    }
+    
     res.setHeader('Content-Type','text/html');
     res.write('<html>')
     res.write('<head><title>Node App</title><head>')
