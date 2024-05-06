54abc0fc598fab6364809398906323b912baf84a
ParvathySree <sreeparvathylaiju@gmail.com>
2024-05-06 15:51:41 +0530

    Routing the requests

--- app.js
+++ app.js
@@ -2,11 +2,18 @@ const http = require('http');
 
 const server = http.createServer((req,res)=>{
     console.log(req.url,req.method,req.headers);
+    if(req.url==='/'){
+        res.setHeader('Content-Type','text/html');
+        res.write('<html>')
+        res.write('<head><title>Node App</title><head>')
+        res.write('<body><form action="/message" method="POST"><input type="text" name "message"><button type="submit">Send</button></form><body>')
+        res.write('</html>')
+        return res.end();
+    }
     res.setHeader('Content-Type','text/html');
     res.write('<html>')
     res.write('<head><title>Node App</title><head>')
     res.write('<body><h1>Welcome to my app</h1><body>')
-    res.write('<html>')
     res.write('</html>')
     res.end();
 })
