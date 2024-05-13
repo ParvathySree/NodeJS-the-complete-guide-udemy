59650232027f1f42543f9131f618339a0325ba75
ParvathySree <sreeparvathylaiju@gmail.com>
2024-05-13 11:54:35 +0530

    pug files

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
-const express = require('express')
-const app = express();
+const path = require('path');
+
+const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path')
+
+const app = express();
 
 app.engine('pug', require('pug').__express)
-app.set('view engine','pug');
-app.set('views','views')
+app.set('view engine', 'pug');
+app.set('views', 'views');
 
-const rootDir = require('./utils/path')
-const adminData = require('./routes/admin')
-const shopRoutes = require('./routes/shop')
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(express.static(path.join(rootDir,'public')))
-app.use('/admin',adminData.routes);
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
-app.use((req,res,next)=>{
-    res.status(404).sendFile(path.join(rootDir,'views','page-not-found.html'))
-})
+app.use((req, res, next) => {
+    res.status(404).render('404', {pageTitle: 'Page Not Found'});
+});
 
-app.listen(3000,()=>{
-    console.log("Server started at port 3000")
-})
+app.listen(3000);
