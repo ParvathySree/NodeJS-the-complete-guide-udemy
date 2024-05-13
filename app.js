1a5c024d6edee47c5b99589bb49f1c1cdc14d517
ParvathySree <sreeparvathylaiju@gmail.com>
2024-05-13 17:40:28 +0530

    Controllers

--- app.js
+++ app.js
@@ -5,22 +5,19 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.engine('pug', require('pug').__express)
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminData = require('./routes/admin');
+const errorController = require('./controllers/error')
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Page Not Found'});
-});
+app.use(errorController.get404);
 
 app.listen(3000);
